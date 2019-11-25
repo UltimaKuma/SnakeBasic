87186bbe266d814cfcd71a0149ed8577e3f70995
UltimaKuma <chinl@outlook.co.nz>
2019-11-25 18:15:36 +1300

    Rendered apple after game over

--- snake.js
+++ snake.js
@@ -3,15 +3,15 @@ const context = canvas.getContext("2d");
 
 const box = 32;
 
-let snake =[];
+let snake = [];
 snake[0] = {
-    x : 9 * box,
-    y : 10 * box
+    x: 9 * box,
+    y: 10 * box
 }
 
 let food = {
-    x : Math.floor(Math.random()*17+1) * box,
-    y : Math.floor(Math.random()*17+3) * box,
+    x: Math.floor(Math.random() * 17 + 1) * box,
+    y: Math.floor(Math.random() * 17 + 3) * box,
 }
 
 let score = 0;
@@ -20,89 +20,91 @@ document.addEventListener("keydown", direction);
 
 let d;
 
-function direction(event){
+function direction(event) {
     //dont allow movement in the opposite direction
-    switch(event.keyCode){
+    switch (event.keyCode) {
         case 37:
-            d = (d=="RIGHT")? d : "LEFT";
+            d = (d == "RIGHT") ? d : "LEFT";
             break;
         case 38:
-            d = (d=="DOWN")? d : "UP";
+            d = (d == "DOWN") ? d : "UP";
             break;
         case 39:
-            d = (d=="LEFT")? d : "RIGHT";
+            d = (d == "LEFT") ? d : "RIGHT";
             break;
         case 40:
-            d = (d=="UP")? d : "DOWN";
+            d = (d == "UP") ? d : "DOWN";
             break;
         default:
     }
 }
 
-function collision(head, array){
-    for(let i = 0; i<array.length; i++){
-        if(head.x == array[i].x && head.y == array[i].y){
+function collision(head, array) {
+    for (let i = 0; i < array.length; i++) {
+        if (head.x == array[i].x && head.y == array[i].y) {
             return true;
         }
     }
     return false;
 }
 
-function draw(){
+function draw() {
     //draw play area
     context.fillStyle = "#24252A";
     context.fillRect(0, 0, 608, 672);
     context.fillStyle = "darkgray";
-    context.fillRect(box, 3*box, 17*box, 17*box);
+    context.fillRect(box, 3 * box, 17 * box, 17 * box);
 
     //draw snake
-    for (let i = 0; i < snake.length; i++){
-        context.fillStyle = ( i==0 )? "darkgreen" : "green";
+    for (let i = 0; i < snake.length; i++) {
+        context.fillStyle = (i == 0) ? "darkgreen" : "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    // if apple is collected, do not pop the snake
+    if (snakeX == food.x && snakeY == food.y) {
+        score++;
+        food = {
+            x: Math.floor(Math.random() * 17 + 1) * box,
+            y: Math.floor(Math.random() * 17 + 3) * box,
+        }
+    } else {
+        snake.pop();
     }
 
     //draw apple
     context.fillStyle = "red";
     context.fillRect(food.x, food.y, box, box);
 
-    let snakeX = snake[0].x;
-    let snakeY = snake[0].y;
-
-    if(snakeX == food.x && snakeY == food.y){
-        score++;
-        food = {
-            x : Math.floor(Math.random()*17+1) * box,
-            y : Math.floor(Math.random()*17+3) * box,
-        }
-    }else{
-        snake.pop();
-    }
-
-    
-
-    if( d == "LEFT" ) snakeX -= box;
-    if( d == "UP" ) snakeY -= box;
-    if( d == "RIGHT" ) snakeX += box;
-    if( d == "DOWN" ) snakeY += box;
+    if (d == "LEFT") {snakeX -= box;}
+    else if (d == "UP") {snakeY -= box;}
+    else if (d == "RIGHT") {snakeX += box;}
+    else if (d == "DOWN") {snakeY += box;}
 
     let newHead = {
-        x : snakeX,
-        y : snakeY
+        x: snakeX,
+        y: snakeY
     }
 
     //game over
-    if(snakeX<box || snakeX>17*box || snakeY < 3*box || snakeY>19*box || collision(newHead, snake)){
+    if (snakeX < box || snakeX > 17 * box || snakeY < 3 * box || snakeY > 19 * box || collision(newHead, snake)) {
         clearInterval(game);
+        context.fillStyle = "white";
+        context.font = "45px Calibri";
+        context.fillText("GAME OVER", 6 * box, 10 * box);    
     }
-    
+
     snake.unshift(newHead);
 
     //draw score
     context.fillStyle = "red";
-    context.fillRect(1*box, 0.75*box, box, box);
+    context.fillRect(1 * box, 0.75 * box, box, box);
     context.fillStyle = "white";
     context.font = "45px Calibri";
-    context.fillText(score, 2.5*box, 1.7*box);
+    context.fillText(score, 2.5 * box, 1.7 * box);
 }
 
 let game = setInterval(draw, 100);
